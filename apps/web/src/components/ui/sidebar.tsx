53b2a483a097a2544717ce136a46eb2a6a182cf2
Daniel Abgottspon <daniel.abgottspon@outlook.de>
2026-05-30 20:05:07 +0200

    Ignore biome warning and comment why

--- apps/web/src/components/ui/sidebar.tsx
+++ apps/web/src/components/ui/sidebar.tsx
@@ -77,7 +77,7 @@ function SidebarProvider({
         _setOpen(openState);
       }
 
-      // This sets the cookie to keep the sidebar state.
+      // biome-ignore lint: Cookie Store API is async and incompatible with this sync callback
       document.cookie = `${SIDEBAR_COOKIE_NAME}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`;
     },
     [setOpenProp, open],
